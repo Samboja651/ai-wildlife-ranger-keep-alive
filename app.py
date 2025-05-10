f7371a1638debf25b7c6fde26785d1f408be4586
Samboja651 <waribekihiko651@gmail.com>
2025-05-10 15:33:02 +0300

    update ping links

--- app.py
+++ app.py
@@ -13,7 +13,9 @@ app = Flask(__name__)
 
 
 # endpoint to ai wildlife ranger
-APP_PING_URL = os.getenv('PING_URL')
+PING_ARTS_EXPERIENCE = os.getenv('PING_ARTS_EXPERIENCE')
+PING_AI_WILDLIFE_RANGER = os.getenv('PING_AI_WILDLIFE_RANGER')
+
 LOG_FILE = "keep_alive_log.txt"
 
 def log_message(message):
@@ -24,17 +26,36 @@ def log_message(message):
         log_file.flush()  # Ensure logs are written immediately
     print(f"[{timestamp}] {message}")  # Print to console for debugging
 
-def ping_app():
-    """Send a request to the app.py server to keep it alive."""
+def ping_servers():
+    """Send a request to each app server to keep them alive."""
     while True:
         try:
-            response = requests.get(APP_PING_URL, timeout=300)
-            if response.status_code == 200:
-                message = f"Ping to arts-experience was successful. Response: {response.json().get('message')}"
-                log_message(message)
+            res_arts_experience = requests.get(PING_ARTS_EXPERIENCE, timeout=10)
+            res_ai_wildlife_ranger = requests.get(PING_AI_WILDLIFE_RANGER, timeout=10)
+            if res_arts_experience.status_code == 200 and res_ai_wildlife_ranger.status_code == 200:
+                message_arts = f"Ping to arts-experience was successful. Response: {res_arts_experience.json().get('message')}"
+                print(message_arts)
+                log_message(message_arts)
+
+                message_ranger = f"Ping to ai-wildlife-ranger was successful. Response: {res_ai_wildlife_ranger.json().get('message')}"
+                print(message_ranger)
+                log_message(message_ranger)
+            elif res_arts_experience.status_code == 200 or res_ai_wildlife_ranger.status_code == 200:
+                message_arts = f"Ping to arts-experience was successful. Response: {res_arts_experience.json().get('message')}"
+                print(message_arts)
+                log_message(message_arts)
+
+                message_ranger = f"Ping to ai-wildlife-ranger was successful. Response: {res_ai_wildlife_ranger.json().get('message')}"
+                print(message_ranger)
+                log_message(message_ranger)
             else:
-                message = f"Either the app server is restarting or is busy: {response.status_code}"
-                log_message(message)
+                message_arts = f"Either the app server is restarting or is busy: {res_arts_experience.status_code}"
+                print(message_arts)
+                log_message(message_arts)
+
+                message_ranger = f"Either the app server is restarting or is busy: {res_ai_wildlife_ranger.status_code}"
+                print(message_ranger)
+                log_message(message_ranger)
         except requests.RequestException as e:
             message = f"Failed to reach app.py. Network Issue: {e}"
             log_message(message)
@@ -44,12 +65,12 @@ def ping_app():
 
 @app.route('/ping', methods=['GET'])
 def handle_ping():
-    """Handle ping requests"""
+    """Handle incomig ping requests"""
     log_message("Received ping from arts-experience: Active")
     return jsonify({"message": "Keep Alive Worker is active"}), 200
 
-# Start the ping_app function as a separate process
-ping_process = Process(target=ping_app)
+# Start the ping app servers function as a separate process
+ping_process = Process(target=ping_servers)
 ping_process.start()
 
 if __name__ == "__main__":
